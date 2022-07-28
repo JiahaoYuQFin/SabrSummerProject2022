75ff7ea07a4bf3a7b14967f5094e9623183faacf
wynnchan <wchencj@connect.ust.hk>
2022-07-28 10:01:22 +0800

    Update Hedge Module

--- 02_src/HedgingModel/hedge.py
+++ 02_src/HedgingModel/hedge.py
@@ -19,8 +19,11 @@ def Put_Call_Parity_l(df, by_rank = 1):
     '''
     parity = []
     for strike in set(df['strike price']):
-        if df.loc[df['strike price'] == strike, 'signal'].prod() <= 0:
+        if (np.sum(df['strike price'] == strike) == 2) and (df.loc[df['strike price'] == strike, 'signal'].prod() <= 0):
             parity.append(df.loc[df['strike price'] == strike, :])
+    if parity == []:
+        df['profit_position'] = 0
+        return df
     parity = pd.concat(parity)
     parity_c = parity.loc[parity['type'] == 'C', :]
     parity_c = parity_c.loc[parity_c['signal'] < 0, :]
@@ -35,7 +38,7 @@ def Put_Call_Parity_l(df, by_rank = 1):
         parity_p['profit_position'] = -1
     parity_l = pd.concat([parity_c, parity_p])
     
-    return pd.merge(df, parity_l[['time', 'code', 'profit_position']], on = ['time', 'code'], how = 'left')
+    return pd.merge(df, parity_l[['time', 'code', 'profit_position']], on = ['time', 'code'], how = 'left').reset_index(drop = True)
 
 def Put_Call_Parity_s(df, by_rank = 1):
     '''
@@ -43,9 +46,12 @@ def Put_Call_Parity_s(df, by_rank = 1):
     if by_rank = 100(a relatively large number), trade all put call parities that satify condition
     '''
     parity = []
-    for strike in df['strike price']:
-        if df.loc[df['strike price'] == strike, 'signal'].prod() <= 0:
+    for strike in set(df['strike price']):
+        if (np.sum(df['strike price'] == strike) == 2) and (df.loc[df['strike price'] == strike, 'signal'].prod() <= 0):
             parity.append(df.loc[df['strike price'] == strike, :])
+    if parity == []:
+        df['profit_position'] = 0
+        return df
     parity = pd.concat(parity)
     parity_c = parity.loc[parity['type'] == 'C', :]
     parity_c = parity_c.loc[parity_c['signal'] > 0, :]
@@ -60,7 +66,7 @@ def Put_Call_Parity_s(df, by_rank = 1):
         parity_p['profit_position'] = 1
     parity_s = pd.concat([parity_c, parity_p])
     
-    return pd.merge(df, parity_s[['time', 'code', 'profit_position']], on = ['time', 'code'], how = 'left')
+    return pd.merge(df, parity_s[['time', 'code', 'profit_position']], on = ['time', 'code'], how = 'left').reset_index(drop = True)
     
     
 def Straddle(df, by_rank = 1):
@@ -99,7 +105,7 @@ def Bull_Call_Spread(df, by_rank = 1):
     call_ls = pd.concat([call_long.loc[call_long['signal'].nsmallest(by_rank).index, :], call_short.loc[call_short['signal'].nlargest(by_rank).index, :]])
     call_ls['profit_position'] = np.sign(call_ls['signal'])
     
-    return pd.merge(df, call_ls[['time', 'code', 'profit_position']], on = ['time', 'code'], how = 'left')
+    return pd.merge(df, call_ls[['time', 'code', 'profit_position']], on = ['time', 'code'], how = 'left').reset_index(drop = True)
 
 
 def Bull_Put_Spread(df):
@@ -151,17 +157,23 @@ before use functions below, plz use this function
 '''
 
 def Hedge_Transform(df, spot):
-    global exist_position
-    
     spot['type'] = 'S'
     spot['delta'] = 1
     df = pd.concat([df, spot])
     df['hedge_position'] = 0
     df = df.fillna(0)
-    time0 = df['time'].values[0]
-    exist_position = df.loc[df['time'] == time0, :]
     
     return df    
+
+def Global_Exist(df):
+    global exist_position
+    
+    time0 = df['time'].sort_values().values[0]
+    exist_position = df.loc[df['time'] == time0, :]
+    exist_position['profit_position'] = 0
+    
+    return 0
+    
 
 '''
 控制开仓数量
@@ -171,21 +183,22 @@ def Hedge_Transform(df, spot):
 def Option_Position_Hold(df, quota = 0.5, limit = 1e6):
     global exist_position
     
-    df['current_position'] = exist_position['profit_position'].values
+    exist_position = exist_position.rename(columns = {'profit_position':'current_position'})
+    df = pd.merge(df, exist_position[['code', 'current_position']], how = 'left', on = 'code')
     df['position_available'] = np.abs(df['profit_position'] * df['volume'] * quota)
-    position = df.loc[df['profit_position'] != 0, :].groupby('time')['position_available'].min()
-    position = position.reset_index()
-    df = pd.merge(df.drop(columns = 'position_available'), position, on = 'time')
-    df['profit_position'] = df['profit_position'] * df['position_available']
+    position = df.loc[df['profit_position'] != 0, 'position_available'].min()
+    df['profit_position'] = df['profit_position'] * position
     df.loc[(df['profit_position']*df['current_position'])>0, 'profit_position'] = df.loc[(df['profit_position']*df['current_position'])>0, 'current_position']
     df = df.drop(columns=['position_available', 'current_position'])
     
-    exist_position = df
+    exist_position = df.copy()
     
     return df
 
 # add position gradually
-
+def Option_Position_Add(df, quota = 0.2, limit = 1e6):
+    
+    return df
 
 '''
 对冲部分
@@ -207,7 +220,7 @@ def Hedge_Spot(df, delta_tolerance = 0):
     else:
         df.loc[df['type']=='S', 'hedge_position'] = exist_position['hedge_position'].sum()
     
-    exist_position = df
+    exist_position = df.copy()
     
     return df
 
@@ -219,22 +232,26 @@ def Hedge_ATM(df, atm = 0.2, delta_tolerance = 0):
     '''
     global exist_position
     
-    df['hedge_position'] = exist_position['hedge_position'].values
+    spot = df.loc[df['type'] == 'S', 'close'].values[0]
+    exist_position = exist_position.rename(columns = {'hedge_position':'current_position'})
+    df = pd.merge(df, exist_position[['code', 'current_position']], how = 'left', on = 'code')
+    df['hedge_position'] = df['current_position']
+    df = df.drop(columns = 'current_position')
     df.loc[df['hedge_position']*df['profit_position'] != 0, 'hedge_position'] = 0
-    df.loc[np.abs(df.loc[df['hedge_position'] != 0, 'delta'] - 0.5) > atm, 'hedge_position'] = 0
+    df.loc[np.abs(df['delta'] - 0.5) > atm, 'hedge_position'] = 0
     delta = ((df['hedge_position'] + df['profit_position']) * df['delta']).sum()
     hedge_delta = (df['hedge_position'] * df['delta']).sum()
-    if np.abs(hedge_delta) < np.abs(delta):
+    if np.sign(hedge_delta) != np.sign(delta):
         df['hedge_position'] = 0
     delta = ((df['hedge_position'] + df['profit_position']) * df['delta']).sum()
-    if np.abs(delta) < delta_tolerance:
+    if np.abs(delta) <= delta_tolerance:
         pass
     else:
         instruments = df.loc[delta/df['delta']<0, :]
-        hedge_option = np.abs(instruments['strike price'] - instruments['spot']).argmin()
-        df.loc[hedge_option, 'hedge_position'] = delta/df.loc[hedge_option, 'delta']
+        hedge_option = np.abs(instruments['strike price'] - spot).argmin()
+        df.loc[hedge_option, 'hedge_position'] = -delta/df.loc[hedge_option, 'delta']
     
-    exist_position = df
+    exist_position = df.copy()
     
     return df
 
